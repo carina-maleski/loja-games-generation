3cae30f7ac42cdb3491692e317c7bafa1f050344
carina-maleski <carinamaleski@gmail.com>
2024-01-31 12:12:32 -0300

    Correção do banco de dados e método put de Produto

--- lojagames/src/main/java/com/generation/lojagames/controller/ProdutoController.java
+++ lojagames/src/main/java/com/generation/lojagames/controller/ProdutoController.java
@@ -61,7 +61,7 @@ public class ProdutoController {
 
 	@PutMapping
 	public ResponseEntity<Produto> put (@Valid @RequestBody Produto produto){
-		if(produtoRepository.existsById(produto.getCategoria().getId())) {
+		if(produtoRepository.existsById(produto.getId())) {
 			if(categoriaRepository.existsById(produto.getCategoria().getId()))
 				return ResponseEntity.status(HttpStatus.OK)
 						.body(produtoRepository.save(produto));
